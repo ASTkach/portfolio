0640d117b1807f2d9de249a0d756d0aaa5507f3b
ASTkach <anatolii.s.tkach@gmail.com>
2023-08-10 18:35:26 +0300

    Added trim method to headers

--- js/modules/moving-letters.js
+++ js/modules/moving-letters.js
@@ -1,20 +1,20 @@
 const movingLetters = () => {
-    const titles = Array.from(document.querySelectorAll(".letters"));
+    const titles = Array.from(document.querySelectorAll('.letters'));
 
     setTimeout(() => {
         titles.forEach((title) => {
-            title.classList.add("_visibl");
+            title.classList.add('_visibl');
         });
 
         const titleContent = titles.map((word) => {
-            const splitText = word.textContent.split("");
+            const splitText = word.textContent.trim().split('');
             return splitText.map((letter, index) => {
                 return `<span style="--order:${index + 1}">${letter}</span>`;
             });
         });
 
         titleContent.map((array, index) => {
-            document.querySelector(`.letters${index}`).innerHTML = array.map((el) => el).join("");
+            document.querySelector(`.letters${index}`).innerHTML = array.map((el) => el).join('');
         });
     }, 100);
 };
