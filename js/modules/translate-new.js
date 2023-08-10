4dc747e71fd138883d6b10d8b3dd7745c64acdcf
ASTkach <anatolii.s.tkach@gmail.com>
2023-08-10 17:42:13 +0300

    Translation of new projects added

--- js/modules/translate-new.js
+++ js/modules/translate-new.js
@@ -1,33 +1,33 @@
 const langArr = {
     title: {
-        en: "My portfolio",
+        en: 'My portfolio',
     },
     home: {
-        en: "home",
+        en: 'home',
     },
     about: {
-        en: "about",
+        en: 'about',
     },
     projects: {
-        en: "projects",
+        en: 'projects',
     },
     contact: {
-        en: "contact",
+        en: 'contact',
     },
     greetings: {
         en: "Hi, I'm",
     },
     name: {
-        en: "Anatolii Tkach",
+        en: 'Anatolii Tkach',
     },
     slogan: {
-        en: "I have a lot of zeal and want to apply it in new projects.",
+        en: 'I have a lot of zeal and want to apply it in new projects.',
     },
     getInTouch: {
-        en: "Contact me",
+        en: 'Contact me',
     },
     aboutMe: {
-        en: "About me",
+        en: 'About me',
     },
     aboutMePart1: {
         en: `Hello again! I'm a Frontend Developer located in Kyiv, Ukraine. I have a higher technical education.
@@ -50,52 +50,66 @@ const langArr = {
         technologies. The level of knowledge of the English language is B1.`,
     },
     myProjects: {
-        en: "My projects",
+        en: 'My projects',
     },
     firstProject: {
+        en: `The application was created using the React library. In it, the user can write down the books he plans to read.
+        User can add books to separate categories: currently reading, read, and deleted. It is also possible to search for 
+        books by title or author.`,
+    },
+    secondProject: {
+        en: `A real project that remains frozen for the time being. The project includes navigation on the page, SVG animation
+        and scrolling animation. In this project are used CSS variables, JavaScript modules, and Icon Font.`,
+    },
+    thirdProject: {
+        en: `Website of a cleaning company. A real project that is in the process of being launched. The project includes the possibility
+        of selecting the desired service and dynamic display of this action. In this project are used CSS variables, JavaScript modules,
+        Swiper Slider and Icon Font.`,
+    },
+    fourthProject: {
         en: `Landing page of the NFT market with the possibility of choosing a color scheme. The page implements
         such interactive objects as a form, a slider, and a modal window. In this project are used CSS variables, JavaScript modules, 
         Swiper Slider, Google Fonts and Icon Font. Uses The Rick and Morty REST API based on the Rick and Morty cartoon series.
         There is also an animation of the digital counter.`,
     },
-    secondProject: {
+    fifthProject: {
         en: `Mamore pair game based on the world famous game Mortal Combat. It has three difficulty levels and contains some 
         interactivity borrowed from the original. The game is developed by my own design. Do not delay and choose your destiny.`,
     },
-    thirdProject: {
-        en: `Landing page of VPN service with page navigation, animated svg images and fixed header. In this project are used CSS 
-        variables, JavaScript modules, Swiper Slider, Google Fonts and Icon Font. There is an animation of the digital counter.`,
-    },
+    // thirdProject: {
+    //     en: `Landing page of VPN service with page navigation, animated svg images and fixed header. In this project are used CSS
+    //     variables, JavaScript modules, Swiper Slider, Google Fonts and Icon Font. There is an animation of the digital counter.`,
+    // },
     projectLive: {
-        en: "Live",
+        en: 'Live',
     },
     projectCode: {
-        en: "Code",
+        en: 'Code',
     },
     location: {
-        en: "Kyiv",
+        en: 'Kyiv',
     },
     formName: {
-        en: "First name & Last name",
+        en: 'First name & Last name',
     },
     formEmail: {
-        en: "Email",
+        en: 'Email',
     },
     formMessage: {
-        en: "Message",
+        en: 'Message',
     },
     sendForm: {
-        en: "Submit",
+        en: 'Submit',
     },
     contactMe: {
-        en: "Contact me",
+        en: 'Contact me',
     },
     contactMeText: {
         en: `If you have projects in which I can be useful to you, then use the form to send me a private
         message or contact me in another convenient way for you.`,
     },
     created: {
-        en: "Designed and created by",
+        en: 'Designed and created by',
     },
 };
 
