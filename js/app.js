b6874142b69b15eb69bef1b3177bbb9f492ec2d8
ASTkach <anatolii.s.tkach@gmail.com>
2023-08-11 11:30:44 +0300

    Added 'load' event listener on window

--- js/app.js
+++ js/app.js
@@ -1,9 +1,14 @@
-import navigationManipulation from "./modules/navigation-manipulation.js";
-import movingLetters from "./modules/moving-letters.js";
-import ticTacToe from "./modules/game.js";
-import langSelection from "./modules/lang-selection.js";
+import navigationManipulation from './modules/navigation-manipulation.js';
+import movingLetters from './modules/moving-letters.js';
+import ticTacToe from './modules/game.js';
+import langSelection from './modules/lang-selection.js';
 
 navigationManipulation();
 movingLetters();
 ticTacToe();
 langSelection();
+
+window.addEventListener('load', () => {
+    const wrapper = document.querySelector('.wrapper');
+    wrapper.classList.add('_visibl');
+});
